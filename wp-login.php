d3d0adbeb1e54fe918b7c9edeabcfb2687042d52
Richard Barlow <richard@richardbarlow.co.uk>
2019-01-09 18:13:06 +0000

    Add static site
    Exported from Wordpress site by SimplyStatic

---
+++ wp-login.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Six Degrees of Procrastination &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='/wp-content/plugins/slickr-flickr/lightbox/js/jquery.lightbox-0.5.min.js?ver=1.37'></script>
+<script type='text/javascript' src='/wp-content/plugins/slickr-flickr/galleria/galleria.min.js?ver=1.37'></script>
+<script type='text/javascript' src='/wp-content/plugins/slickr-flickr/slickr-flickr.min.js?ver=1.37'></script>
+<script type="text/javascript">
+jQuery.noConflict(); jQuery(document).ready(function() {
+slickr_flickr_start();
+});
+</script>
+<link rel='stylesheet' id='lightbox-css'  href='/wp-content/plugins/slickr-flickr/lightbox/css/jquery.lightbox-0.5.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='galleria-css'  href='/wp-content/plugins/slickr-flickr/galleria/galleria.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='slickr-flickr-css'  href='/wp-content/plugins/slickr-flickr/slickr-flickr.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='/wp-includes/css/dashicons.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='/wp-includes/css/buttons.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='/wp-admin/css/forms.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='/wp-admin/css/l10n.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='/wp-admin/css/login.min.css?ver=5.0.2' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/">&larr; Back to Six Degrees of Procrastination</a></p>
+		
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
